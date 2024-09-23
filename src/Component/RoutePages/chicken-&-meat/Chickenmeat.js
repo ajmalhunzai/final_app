9530d0e75f4e67aabab44bda9d1ce45bbce9b9fe
zameerabbas <zameerabbas3435@gmail.com>
2024-09-23 12:11:57 -0700

    chcicken data added

--- src/Component/RoutePages/chicken-&-meat/Chickenmeat.js
+++ src/Component/RoutePages/chicken-&-meat/Chickenmeat.js
@@ -35,21 +35,21 @@ const Chickenmeat = () => {
 
 
       <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {chickenmeat?.data?.map((person) => (
+      {chickenmeat?.map((person) => (
         <li
           key={person.email}
           className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
         >
           <div className="flex flex-1 flex-col p-8">
-            <img alt="" src={person.image} className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" />
-            <h3 className="mt-6 text-sm font-medium text-gray-900">{person.name}</h3>
+            <img alt="" src={person.data.image} className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" />
+            <h3 className="mt-6 text-sm font-medium text-gray-900">{person.data.name}</h3>
             <dl className="mt-1 flex flex-grow flex-col justify-between">
               <dt className="sr-only">Title</dt>
-              <dd className="text-sm text-gray-500">{person.price}</dd>
+              <dd className="text-sm text-gray-500">{person.data.price}</dd>
               <dt className="sr-only">Role</dt>
               <dd className="mt-3">
                 <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                  {person.description}
+                  {person.data.description}
                 </span>
               </dd>
             </dl>
